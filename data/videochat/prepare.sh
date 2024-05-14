fa0c3321b6c8a02f5746c3021bdb587f08870352
Dongfu <dongfu.jdf@gmail.com>
2024-05-14 19:45:08 -0400

    update

--- data/videochat/prepare.sh
+++ data/videochat/prepare.sh
@@ -1,2 +1,4 @@
 gdown https://drive.google.com/uc?id=1Mb45IO7ArEjgdOg140mvSMOMNvIZOXml
+gdown https://drive.google.com/uc?id=1zGRyVSUMoczGq6cjQFmT0prH67bu2wXD
 1Mb45IO7ArEjgdOg140mvSMOMNvIZOXml/view
+1zGRyVSUMoczGq6cjQFmT0prH67bu2wXD/view
